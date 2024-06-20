80c5bbf64c10b5c131300f87e6b7e02484db40c9
Brian Anderson PC <brian.briananderson@gmail.com>
2024-06-20 18:06:20 -0500

    added the coiol new claude model to models claude-3-5-sonnet-20240620

--- Chain.py
+++ Chain.py
@@ -73,7 +73,7 @@ class Chain():
     models = {
         "ollama": [m['name'] for m in ollama.list()['models']],
         "openai": ["gpt-4o","gpt-4-turbo","gpt-3.5-turbo-0125"],
-        "anthropic": ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307"],
+        "anthropic": ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307", "claude-3-5-sonnet-20240620"],
         "google": ["gemini-1.5-pro-latest", "gemini-1.5-flash-latest", "gemini-pro"],
         "groq": ["llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma-7b-it"],
         "testing": ["polonius"]
