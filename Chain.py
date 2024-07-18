a37f16ccf25db1825a99d32e694ede792d5eb58c
Brian Anderson <brian.briananderson@gmail.com>
2024-07-18 12:56:12 -0500

    added openai's new mini model -- gpt-4-mini

--- Chain.py
+++ Chain.py
@@ -124,7 +124,7 @@ class Chain():
 		print('Ollama is not running.')
 	models = {
 		"ollama": ollama_models,
-		"openai": ["gpt-4o","gpt-4-turbo","gpt-3.5-turbo-0125"],
+		"openai": ["gpt-4o","gpt-4-turbo","gpt-3.5-turbo-0125", "gpt-4o-mini"],
 		"anthropic": ["claude-3-opus-20240229", "claude-3-sonnet-20240229", "claude-3-haiku-20240307", "claude-3-5-sonnet-20240620"],
 		"google": ["gemini-1.5-pro-latest", "gemini-1.5-flash-latest", "gemini-pro"],
 		"groq": ["llama3-8b-8192", "llama3-70b-8192", "mixtral-8x7b-32768", "gemma-7b-it"],
@@ -327,6 +327,8 @@ class Model():
 			self.model = 'mistral:latest'                                           # defaulting to the latest mistral model
 		elif model == "haiku":
 			self.model = 'claude-3-haiku-20240307'                                  # haiku model
+		elif model == "gpt-mini":
+			self.model = 'gpt-4o-mini'											    # defaulting to the mini model
 		elif model in list(itertools.chain.from_iterable(Chain.models.values())):   # any other model we support (flattened the list)
 			self.model = model
 		else:
