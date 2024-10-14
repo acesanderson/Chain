6790a815ab87f05d1bb8af5a5ef04793153c857c
Brian Anderson <brian.briananderson@gmail.com>
2024-10-14 17:45:20 -0500

    Created new chain class. Will be creating a Message class next, which will get me back to 80% functionality (minus async and chat).

---
+++ v2/message.py
@@ -0,0 +1,42 @@
+"""
+Helper class for generating different types of messages, message store, etc., for generation and chat.
+"""
+from pydantic import BaseModel
+
+
+class Message(BaseModel):
+	"""
+	Industry standard, more or less, for messaging with LLMs.
+	System roles can have some weirdness (like with Anthropic), but role/content is standard.
+	"""
+	role: str
+	content: str
+
+class Messages(BaseModel):
+	"""
+	Industry standard, more or less, for messaging with LLMs.
+	System roles can have some weirdness (like with Anthropic), but role/content is standard.
+	"""
+	messages: List[Message]
+
+def is_messages_object(input):
+	if not input:
+		return False
+	try:
+		Messages(messages=input)
+		return True
+	except Exception as e:
+		print(e)
+		return False
+
+def create_messages(system_prompt: str, input_variables = None) -> list[dict]:
+	"""
+	Takes a system prompt object (Prompt()) or a string, an optional input object, and returns a list of messages.
+	"""
+	if isinstance(system_prompt, str):
+		system_prompt = Prompt(system_prompt)
+	if input_variables:
+		messages = [{'role': 'system', 'content': system_prompt.render(input_variables=input_variables)}]
+	else:
+		messages = [{'role': 'system', 'content': system_prompt.string}]
+	return messages
