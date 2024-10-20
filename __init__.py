2a231d21861ee083df778efe4bdba546234920c4
Brian Anderson <brian.briananderson@gmail.com>
2024-10-20 18:51:04 -0500

    more munging

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
-from chain.chain import Chain
-from prompt.prompt import Prompt
-from model.model import Model
-from parser.parser import Parser
-from response.response import Response
-from message.message import Message
-from message.messagestore import MessageStore
+from .chain.chain import Chain
+from .prompt.prompt import Prompt
+from .model.model import Model
+from .response.response import Response
+from .parser.parser import Parser
+from .message.message import Message
+from .message.message_store import MessageStore
 
 __all__ = ['Chain', 'Prompt', 'Model', 'Parser', 'Response', 'Message', 'MessageStore']
