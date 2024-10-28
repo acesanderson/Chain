9d2e6591cfae3f3189a89bbb9a7257f9618177ad
bianders <phat@biden.co.uk>
2024-10-28 16:09:02 -0500

    small edits to chain.py

--- chain/chain.py
+++ chain/chain.py
@@ -12,7 +12,8 @@ from ..prompt.prompt import Prompt
 from ..model.model import Model
 from ..response.response import Response
 from ..parser.parser import Parser
-from ..message.message import Message, MessageStore
+from ..message.message import Message
+from ..message.messagestore import MessageStore
 
 
 class Chain:
