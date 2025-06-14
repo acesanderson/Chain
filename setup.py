c061b942a101a7cca14fc960e3db54b6308605c5
Corn Pop <biden@phat.co.uk>
2025-06-14 13:11:43 -0500

    server stuff

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="Chain",
     version="2.5",
-    packages=find_packages(include=["chain", "chain.*"]),
+    packages=find_packages(),
     entry_points={
         "console_scripts": [
             "update_ollama=Chain.scripts.update_ollama_list:main",
