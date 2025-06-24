16d725e035ea0f4752274dbfefe3464df77738d3
bianders <phat@biden.co.uk>
2025-06-24 00:33:49 -0500

    refactor messages.py

--- message/messages.py
+++ message/messages.py
@@ -1,24 +1,43 @@
 from Chain.message.message import Message
 
 
-class Messages:
-    def __init__(self, items: list[Message]):
-        self._messages = items
+class Messages(list):
+    def __init__(self, iterable=None):
+        iterable = iterable or []
+        # Validate everything before initializing
+        for item in iterable:
+            self._validate(item)
+        super().__init__(iterable)
 
-    def __getitem__(self, index):
-        return self._messages[index]
+    def append(self, item):
+        self._validate(item)
+        super().append(item)
 
-    def __len__(self):
-        return len(self._messages)
+    def extend(self, iterable):
+        for item in iterable:
+            self._validate(item)
+        super().extend(iterable)
 
-    def __iter__(self):
-        return iter(self._messages)
+    def insert(self, index, item):
+        self._validate(item)
+        super().insert(index, item)
 
-    def append(self, message: Message):
-        self._messages.append(message)
+    def __setitem__(self, index, value):
+        # Handle slice assignment separately
+        if isinstance(index, slice):
+            for item in value:
+                self._validate(item)
+        else:
+            self._validate(value)
+        super().__setitem__(index, value)
+
+    @staticmethod
+    def _validate(item):
+        if not isinstance(item, Message):
+            raise TypeError(f"Only Message instances are allowed, got: {type(item)}")
 
     def model_dump(self) -> dict:
-        return {}
+        return {"messages": [msg.model_dump() for msg in self]}
 
     def model_dump_json(self) -> str:
-        return ""
+        return "[" + ", ".join(msg.model_dump_json() for msg in self) + "]"
